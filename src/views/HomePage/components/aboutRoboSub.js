a6117251bb32876d5a9af5f6de128794ffe6302e
Nathan JW <njw230004@utdallas.edu>
2024-11-19 03:40:44 -0600

    add image carousel

--- src/views/HomePage/components/aboutRoboSub.js
+++ src/views/HomePage/components/aboutRoboSub.js
@@ -1,13 +1,31 @@
 import '../styles/aboutRoboSub.css'
-import aboutPhoto from '../images/GroupPhoto.jpeg'
-function AboutRoboSub(){
+import aboutPhoto from '../images/GroupPhotos/GroupPhoto.jpeg'
+import awardPhoto from '../images/GroupPhotos/DSC04824.jpg'
+import jesuitPhoto from '../images/GroupPhotos/DSC04352.JPG'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+function AboutRoboSub() {
     return(
         <div id="AboutRobo" className='about'>
-            <img className="TeamImage" src={aboutPhoto} alt="groupPhoto"/>
-            <div className="AboutTextBox">
-                <h1>About RoboSub</h1>
-                <p>
-                RoboSub is an organization at UTD focused on the development of marine robotics. 
+          <Carousel showStatus={false} autoPlay={true} showIndicators={false} showArrows={false} infiniteLoop={true}>
+            <div>
+              <img className="TeamImage" src={aboutPhoto} alt="groupPhoto"/>
+              <p>Hydromeda Team 2023-2024 with Crush Depth post award ceremony</p>
+            </div>
+            <div>
+              <img className="TeamImage" src={awardPhoto} alt="groupPhoto"/>
+              <p>Hydromeda Team 2023-2024 during award ceremony</p>
+            </div>
+            <div>
+              <img className="TeamImage" src={jesuitPhoto} alt="groupPhoto"/>
+              <p>Hydromeda Team 2023-2024 speaking with other competitors from Jesuit High School</p>
+            </div>
+          </Carousel>
+          <div className="AboutTextBox">
+            <h1>About RoboSub</h1>
+            <p>
+              RoboSub is an organization at UTD focused on the development of marine robotics.
                 We seek to tap into the excitement and creativity seen in robotics teams, and in RoboSub, 
                 we work with the added challenge of a marine environment.
                 RoboSub's mission is to bring together individuals 
