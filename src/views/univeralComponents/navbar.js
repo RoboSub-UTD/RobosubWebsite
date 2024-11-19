e8ead1c3263758471e4488b5556fba2ccdf538a8
Nathan JW <njw230004@utdallas.edu>
2024-11-18 23:49:33 -0600

    Navigate to home page when clicking logo

--- src/views/univeralComponents/navbar.js
+++ src/views/univeralComponents/navbar.js
@@ -12,7 +12,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 function Navbar() {
   const [user, setUser] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
-  
+
   const goTo = (URL) => {
     window.location.href = URL
   }
@@ -72,79 +72,80 @@ function Navbar() {
   }
 
   return (
-    <div>
-      <nav className='navbar'>
-        <div className="navbar-content">
-          <div className='left'>
-            <img className='logo-img' src={logo} alt="RoboSub Logo" />
-            <h1 className='Title'>RoboSub UTD</h1>
-          </div>
+      <div>
+        <nav className='navbar'>
+          <div className="navbar-content">
+            <div className='left' unselectable={"on"} onClick={() => {
+              if (isHomePage) {
+                scrollToSection('Home');
+              }
+              else {
+                goTo('/');
+              }
+            }}>
+              <img className='logo-img' src={logo} alt="RoboSub Logo" />
+              <h1 className='Title'>RoboSub UTD</h1>
+            </div>
 
-          <div className='right'>
-            <ul className='navbar-menu'>
-              <li className='navbar-item'>
-                <button onClick={() => scrollToSection('Home')}>
-                  <a className='navbar-item' href="#/">Home</a>
-                </button>
-              </li>
-              
-              {isHomePage && (
-                <>
-                  <li className='navbar-item'>
-                    <button onClick={() => scrollToSection('AboutRobo')}>About</button>
-                  </li>
-                  <li className='navbar-item'>
-                    <button onClick={() => scrollToSection('Officers')}>Officers </button>
-                  </li>
-                </>
-              )}
+            <div className='right'>
+              <ul className='navbar-menu'>
+                {isHomePage && (
+                    <>
+                      <li className='navbar-item'>
+                        <button onClick={() => scrollToSection('AboutRobo')}>About</button>
+                      </li>
+                      <li className='navbar-item'>
+                        <button onClick={() => scrollToSection('Officers')}>Officers </button>
+                      </li>
+                    </>
+                )}
 
-              <li className="navbar-item projects-menu">
-                <button onClick={() => scrollToSection('Projects')} className="dropdown-button"><a href='#/' className='navbar-item'>Projects</a> <span className="arrow">&#9662;</span></button>
-                <div className='dropdown-container'>
-                  <ul className="dropdown-menu">
-                    <li onClick={()=>goTo("#/projects/MateROV")}>MateROV</li>
-                    <li onClick={()=>goTo("#/projects/FloatTube")} >Float Tube</li>
-                    <li onClick={()=>goTo("#/projects/MiniFloat")}>Mini Float</li>
-                    <li onClick={()=>goTo("#/projects/Roboboat")}>RoboBoat</li>
-                  </ul>
-                </div>
-              </li>
-              {isHomePage && (
-                  <li className='navbar-item'>
-                    <button onClick={() => scrollToSection('Sponsors')}>Sponsors</button>
-                  </li>
-              )}
-
-              {isAdmin && (
-                <li className='navbar-item'>
-                  <button>
-                    <a className='navbar-item' href="#/Admin">Admin</a>
-                  </button>
+                <li className="navbar-item projects-menu">
+                  <button onClick={() => scrollToSection('Projects')} className="dropdown-button"><a href='#/' className='navbar-item'>Projects</a> <span className="arrow">&#9662;</span></button>
+                  <div className='dropdown-container'>
+                    <ul className="dropdown-menu">
+                      <li onClick={()=>goTo("#/projects/MateROV")}>MateROV</li>
+                      <li onClick={()=>goTo("#/projects/FloatTube")} >Float Tube</li>
+                      <li onClick={()=>goTo("#/projects/MiniFloat")}>Mini Float</li>
+                      <li onClick={()=>goTo("#/projects/Roboboat")}>RoboBoat</li>
+                    </ul>
+                  </div>
                 </li>
-              )}
-              {/* <button onClick={() => { console.log(auth.currentUser); }}>get user</button> */}
+                {isHomePage && (
+                    <li className='navbar-item'>
+                      <button onClick={() => scrollToSection('Sponsors')}>Sponsors</button>
+                    </li>
+                )}
 
-              {user === null ? (
+                {isAdmin && (
+                    <li className='navbar-item'>
+                      <button>
+                        <a className='navbar-item' href="#/Admin">Admin</a>
+                      </button>
+                    </li>
+                )}
+                {/* <button onClick={() => { console.log(auth.currentUser); }}>get user</button> */}
+
+                {user === null ? (
                     <li className='navbar-item'>
                       <button>
                         <a className='navbar-item' href="#/Login">Login</a>
                       </button>
                     </li>
-                  ) : (
+                ) : (
                     <li className='navbar-item'>
                       <button onClick={logOut}>Logout</button>
                     </li>
-                  )}
-            </ul>
-            <button className="hamburger" onClick={toggleSidebar}>
-              ☰
-            </button>
+                )}
+              </ul>
+              <button className="hamburger" onClick={toggleSidebar}>
+                ☰
+              </button>
+            </div>
           </div>
-        </div>
-      </nav>
-      <SideBar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} scrollToSection={scrollToSection} user={user} logOut={logOut}/>
-    </div>
+        </nav>
+        <SideBar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} scrollToSection={scrollToSection} user={user} logOut={logOut}/>
+      </div>
   );
 }
 
