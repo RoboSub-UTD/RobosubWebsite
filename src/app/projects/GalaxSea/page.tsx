ca331061fb18bf2206ad4a1ec957c877045732cc
Donavyn Fitzgerald <donavynfitzgeraldnlc@gmail.com>
2024-11-20 23:35:28 -0600

    Added GalaxSea product page.

---
+++ src/app/projects/GalaxSea/page.tsx
@@ -0,0 +1,20 @@
+import React, {useEffect} from "react";
+
+export default function RoboBoat(){    
+    useEffect(() => {
+        window.scrollTo(0, 0);  // Scrolls to the top of the page
+      }, []);
+    
+    
+    const ProjectName = "RoboBoat"
+    const aboutText = `
+                    An international competition where teams create autonomous 
+                    robotic boats to navigate through different challenges is an exciting 
+                    showcase of engineering ingenuity, teamwork, and technological innovation. 
+                    This competition gathers participants from various countries, including students, 
+                    professionals, and enthusiasts, to design, build, and program their robotic boats 
+                    to tackle a series of complex tasks in a controlled aquatic environment. 
+                    The event not only fosters international collaboration but also inspires 
+                    a new generation of engineers and researchers passionate about robotics, 
+                    marine technology, and environmental sustainability.`
+}
